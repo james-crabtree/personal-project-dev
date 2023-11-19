97190f7d244a94018305a417767be054bfd2625b
James Crabtree <jamescrabtreenz17@gmail.com>
2023-11-19 14:55:16 +1300

    Set 'canvas' background to white

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react'
 
 function Pixel() {
-  const getRandomColor = () =>
-    `#${Math.floor(Math.random() * 0x1000000)
-      .toString(16)
-      .padStart(6, '0')}`
+  const getRandomColor = () => "#ffffff";
+
 
   const [color, setColor] = useState(getRandomColor())
 
